4bff4e4529acd77664684811c3d79a7d79a91b46
Mikhail Ilin <mikhail.ilin93@gmail.com>
2024-09-02 14:36:53 +0500

    add get_shop_list_by_dishes

--- cookbook.py
+++ cookbook.py
@@ -5,8 +5,9 @@
 # 'measure'
 from contextlib import nullcontext
 from idlelib.iomenu import encoding
-
+import collections
 cook_book = {}
+ingridients = {}
 
 def get_recipes_file(filename):
     state_name = 1
@@ -35,7 +36,41 @@ def get_recipes_file(filename):
             quantity_ingredients = quantity_ingredients - 1
             if quantity_ingredients == 0:
                 state = state_name
+    file.close()
     return cook_book
 
-print(get_recipes_file('recipes.txt'))
+def get_shop_list_by_dishes(dishes, person_count):
 
+    for dish in dishes:
+        for ingridient in cook_book.get(dish):
+
+            ingredient_name = ingridient.get('ingredient_name')
+            measure = ingridient.get('measure')
+            quantity = ingridient.get('quantity')
+
+            if ingridients.get(ingredient_name):
+                # суммировать при одинаковом ингридиенте
+                dict_ingr = ingridients.get(ingredient_name)
+                quantity2 = dict_ingr.get('quantity')
+                quantity = int(quantity) + int(quantity2)
+                ingridients[ingredient_name] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
+            else:
+                ingridients[ingredient_name] = {}
+                ingridients[ingredient_name] = (dict(zip(('measure', 'quantity'),(measure, quantity) )))
+
+    #print(f'\n {ingridients}')
+
+    for ingridient in ingridients.keys():
+            str = ingridients.get(ingridient)
+            quantity = str.get("quantity") * person_count
+            measure = str.get('measure')
+        #ingredient_name = ingridient.keys()
+        #quantity = ingridient.get('quantity')
+            ingridients[ingridient] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
+    #print(ingridients)
+    return ingridients
+
+
+#print(get_recipes_file('recipes.txt'))
+get_recipes_file('recipes.txt')
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
