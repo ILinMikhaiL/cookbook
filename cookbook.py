40dc78a9af6f02ab9376b0a4d2425dc512c468f8
Mikhail Ilin <mikhail.ilin93@gmail.com>
2024-09-12 22:13:46 +0500

    corrected_codestyle_and_not_global_variables_create_functions

--- cookbook.py
+++ cookbook.py
@@ -1,24 +1,25 @@
-# 'name_recipes
-# 'quantity_ingredients'
-# 'ingredient_name'
-# 'quantity'
-# 'measure'
 from contextlib import nullcontext
 from idlelib.iomenu import encoding
 import collections
-cook_book = {}
-ingridients = {}
+
 
 def get_recipes_file(filename):
-    state_name = 1
-    state_quantity_ingredients = 2
-    state_ingredients =3
-
-    state = state_name # текущее состояние
     file = open(filename, encoding='utf-8')
     lines = file.readlines()
+    cook_book = create_dict_cook_book(lines)
+    file.close()
+    return cook_book
+
+
+def create_dict_cook_book(lines):
+    cook_book = {}
+    state_name = 1
+    state_quantity_ingredients = 2
+    state_ingredients = 3
+    state = state_name  # текущее состояние
+
     for line in lines:
-        line=line.strip()
+        line = line.strip()
         if not line: continue
         if state == state_name:
             name_recipes = line
@@ -36,14 +37,30 @@ def get_recipes_file(filename):
             quantity_ingredients = quantity_ingredients - 1
             if quantity_ingredients == 0:
                 state = state_name
-    file.close()
     return cook_book
 
-def get_shop_list_by_dishes(dishes, person_count):
 
+def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = get_recipes_file('recipes.txt')
+    ingridients = get_dict_ingridients(cook_book, dishes)
+    ingredients_for_cooking = get_amount_ingredients(ingridients, person_count)
+    return ingredients_for_cooking
+
+
+def get_amount_ingredients(ingridients, person_count):
+    ingredients_for_cooking = {}
+    for ingridient in ingridients.keys():
+        str = ingridients.get(ingridient)
+        quantity = str.get("quantity") * person_count
+        measure = str.get('measure')
+        ingredients_for_cooking[ingridient] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
+    return ingredients_for_cooking
+
+
+def get_dict_ingridients(cook_book, dishes):
+    ingridients = {}
     for dish in dishes:
         for ingridient in cook_book.get(dish):
-
             ingredient_name = ingridient.get('ingredient_name')
             measure = ingridient.get('measure')
             quantity = ingridient.get('quantity')
@@ -56,21 +73,13 @@ def get_shop_list_by_dishes(dishes, person_count):
                 ingridients[ingredient_name] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
             else:
                 ingridients[ingredient_name] = {}
-                ingridients[ingredient_name] = (dict(zip(('measure', 'quantity'),(measure, quantity) )))
-
-    #print(f'\n {ingridients}')
-
-    for ingridient in ingridients.keys():
-            str = ingridients.get(ingridient)
-            quantity = str.get("quantity") * person_count
-            measure = str.get('measure')
-        #ingredient_name = ingridient.keys()
-        #quantity = ingridient.get('quantity')
-            ingridients[ingridient] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
-    #print(ingridients)
+                ingridients[ingredient_name] = (dict(zip(('measure', 'quantity'), (measure, quantity))))
     return ingridients
 
 
-#print(get_recipes_file('recipes.txt'))
-get_recipes_file('recipes.txt')
-get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+if __name__ == "__main__":
+    cook_book_txt = get_recipes_file('recipes.txt')
+    ingridients_txt = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+
+    print(cook_book_txt)
+    print(ingridients_txt)
